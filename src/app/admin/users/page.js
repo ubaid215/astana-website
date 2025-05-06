4877616190577bfffaff510f89e382662a5ccd76
Muhammad Ubaidullah <ubaidtech274@gmail.com>
2025-05-06 15:17:04 +0500

    hu

--- src/app/admin/users/page.js
+++ src/app/admin/users/page.js
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import connectDB from '@/lib/db/mongodb';
 import Participation from '@/lib/db/models/Participation';
 import UserList from '@/components/admin/tables/UserList';
